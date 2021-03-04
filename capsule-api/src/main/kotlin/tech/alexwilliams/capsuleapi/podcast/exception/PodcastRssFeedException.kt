3a5c5803d7518198306ca99bedd8a7d691050928
Alex Williams <alexwilliams3@protonmail.com>
2021-03-03 19:54:43 -0700

    Adding RssService deserilization functionality

---
+++ capsule-api/src/main/kotlin/tech/alexwilliams/capsuleapi/podcast/exception/PodcastRssFeedException.kt
@@ -0,0 +1,4 @@
+package tech.alexwilliams.capsuleapi.podcast.exception
+
+class PodcastRssFeedException(message: String): RuntimeException(message) {
+}
--- capsule-api/src/main/kotlin/tech/alexwilliams/capsuleapi/podcast/services/RssService.kt
+++ capsule-api/src/main/kotlin/tech/alexwilliams/capsuleapi/podcast/services/RssService.kt
@@ -1,11 +1,30 @@
 package tech.alexwilliams.capsuleapi.podcast.services
 
+import com.thoughtworks.xstream.XStream
+import com.thoughtworks.xstream.XStreamException
 import org.springframework.stereotype.Service
+import tech.alexwilliams.capsuleapi.podcast.config.xml.XstreamInitializer
+import tech.alexwilliams.capsuleapi.podcast.exception.PodcastRssFeedException
+import tech.alexwilliams.capsuleapi.podcast.models.PodcastRssFeed
 
 @Service
 class RssService {
+    private var xStream: XStream? = null
+
+    init {
+        val xStreamInitializer = XstreamInitializer()
+        this.xStream = xStreamInitializer.getXstreamInstance()
+    }
 
     fun fetchRssFeed(rssUrl: String): List<String> {
         return listOf()
+    }
+
+    fun deserializePodcastRssFeed(rssFeed: String): PodcastRssFeed {
+        try {
+            return xStream!!.fromXML(rssFeed) as PodcastRssFeed
+        } catch (ex: XStreamException) { // TODO: add logging framework
+            throw PodcastRssFeedException("Error deserializing podcast rss feed")
+        }
     }
 }
